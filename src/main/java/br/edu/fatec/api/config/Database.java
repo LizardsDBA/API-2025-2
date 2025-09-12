de94428b7f2e3c19c3a39994111536062a1885c8
Fagner Louis <faa.louis@hotmail.com>
2025-09-12 08:47:36 -0300

    chore: ajusta diretório do arquivo api.db para dentro de resources

--- src/main/java/br/edu/fatec/api/config/Database.java
+++ src/main/java/br/edu/fatec/api/config/Database.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class Database {
-    private static final String URL = "jdbc:sqlite:api.db";
+    private static final String URL = "jdbc:sqlite:src/main/resources/db/api.db";
 
     private Database() {}
 
