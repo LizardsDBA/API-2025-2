12cb937a2fcbb1cdd58159383308430853ad14d5
CauaDK <cauamohor10@gmail.com>
2025-09-16 22:35:20 -0300

    feat: Criação da tela de Login e redirecionamento para as seguintes telas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
     opens br.edu.fatec.api to javafx.graphics, javafx.fxml;
     exports br.edu.fatec.api;
+    exports br.edu.fatec.api.controller;
+    opens br.edu.fatec.api.controller to javafx.fxml, javafx.graphics;
 }
